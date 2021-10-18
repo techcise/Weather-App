d4adac88c45d091dcc97fa7f8fc19834e39d6c37
Manas Pathak <manaspathak1212@gmail.com>
2021-10-18 15:36:19 +0530

    The Weather App Final Version - All the functionality and a bit fancy

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 
-let text = document.querySelector('#text');
+let text = document.querySelector('#tempreture');
 let locationText = document.querySelector('#location');
 let myBtn = document.querySelector('.myBtn');
-let container = document.querySelector('.container');
+let weather = document.querySelector('#weather');
+let input = document.querySelector('.city');
+let body = document.body;
+let weatherC = document.querySelector('.weatherContainer');
+let weatherIcon = document.querySelector('.weatherIcon');
 
 myBtn.addEventListener('click', async function(e){
     let city = document.querySelector('.city').value;
@@ -13,17 +17,17 @@ myBtn.addEventListener('click', async function(e){
     else {
         e.preventDefault();
         let getData = await fetch(`http://api.weatherapi.com/v1/current.json?key=fa4d5077ca114c948c492922211710&q=${city}&aqi=yes`);
-        let weatherIcon = document.querySelector('.weatherIcon');
         if(getData.status != 200){
             locationText.innerText = 'Can,t find City';
         }
         else {
             let getJson = await getData.json();
-            text.innerText = getJson.current.temp_c;
+            text.innerText = getJson.current.temp_c + ' C';
             locationText.innerText = getJson.location.name;
             weatherIcon.src = getJson.current.condition.icon;
-            container.append(weatherIcon);
+            weather.innerText = getJson.current.condition.text;
             console.log(getJson);
         }
     }
+    console.log(input.value);
 });
